62042d4538f58f04143fa405d69fcac487d50146
Nandini Gupta <65002182+nandini689@users.noreply.github.com>
2023-04-24 13:21:51 +0530

    CrudOperation

---
+++ CrudOperations/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudOperations.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperations.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
